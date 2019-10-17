749922c5270eb8c114e1dfdc708e86b17880dafc
lstephensca <lstephens@chumneyads.com>
2019-10-17 16:45:57 -0400

    Initializing Cardinal Grid

---
+++ index.php
@@ -0,0 +1,70 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Cardinal Grid</title>
+    <link rel="stylesheet" href="./bundle/dist/css/cardinal.css">
+    <link rel="stylesheet" href="./bundle/dist/css/prism.css">
+
+
+
+</head>
+
+<body>
+
+    <!-- containter -->
+    <div id="ca-container">
+
+        <section>
+            <div class="column">
+                <p>Welcome to the new Chumney css grid, "cardinal". It is currently based on flexbox (but will incorporate features of cssgrid in the near future.</p>
+                <p>To get started, all you need is the following css link in your head tag</p>
+            </div>
+            <div class="column">
+                <pre><code class="language-markup">&lt;link rel="stylesheet" href="https://theautohost.com/_cardinal/bundle/dist/css/cardinal.min.css"&gt;</code></pre>
+
+            </div>
+        </section>
+
+
+        <section>
+            <div class="column">
+                <p>We tried making the structure of the new grid much easier to follow and write than the previous version. All you currently need to create a row is:</p>
+            </div>
+            <div class="column">
+                <pre><code class="language-markup">
+                    <span class="token tag">&lt;section&gt;</span>
+                    <span class="token tag">&lt;/section&gt;</span></code></pre>
+
+            </div>
+        </section>
+
+
+
+
+
+
+
+
+
+
+
+
+        <!-- containter -->
+
+        <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+        <script src="./bundle/dist/js/prism.js"></script>
+        <script src="./bundle/dist/js/cardinal-min.js"></script>
+
+
+
+</body>
+<script type="text/javascript">
+
+
+
+
+</script>
+
+</html>
---
+++ model.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Cardinal Page</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="dist/css/cardinal.css">
+</head>
+
+<body>
+
+    <div id="ca-container">
+        <!-- section dd-->
+        <section>
+            <div class="column">
+                <img src="https://www.theautohost.com/_contentPages/vehicleContentPages/toyota/2019/C-HR/images/header.jpg" />
+            </div>
+        </section>
+        <!-- section -->
+
+        <!-- section -->
+        <section>
+            <div class="column">
+                <a href="$" class="btn toyota">View Inventory</a>
+            </div>
+            <div class="column">
+                <a href="$" class="btn toyota">Vehicle Specials</a>
+            </div>
+            <div class="column">
+                <a href="$" class="btn toyota">Get Pre-Approved</a>
+            </div>
+            <div class="column">
+                <a href="$" class="btn toyota">Value Your Trade</a>
+            </div>
+        </section>
+        <!-- section -->
+
+        <!-- section -->
+        <section>
+            <div class="column">
+                <h1>2019 TOYOTA C-HR</h1>
+                <p>The 2019 Toyota C-HR for sale at Bev Smith Toyota in Fort Pierce, FL – serving Port St. Lucie, Vero Beach, Stuart, Okeechobee, and the entire Treasure Coast – is the dynamic crossover your dreams are made of. It runs on a thrilling 2.0L inline 4-cylinder engine with 144 horsepower and 139 lb.-ft. of torque. Between it and the Continuously Variable Transmission with intelligence and Shift mode, you’ll get a whopping 27 mpg in the city and 31 mpg on the highway.1 That means you can spend more time enjoying each road trip and less time stopping at the gas station. Whether your cruise takes you to the grocery store or across the country, you’ll love how dual-zone climate control and available leather-trimmed seats bring a premium feel. If you have a lot of gear or groceries to pack, lay down the 60/40-split folding rear seats to create plenty of cargo area. You’ll even have standard Toyota Safety Sense™ P features, including dynamic radar cruise control, a pre-collision system, lane departure alerts, and automatic high beams,2 to help protect you and anything you have inside. When you’re backing out of a space, the 8-inch touchscreen display will show an integrated rear backup camera with gridlines to assist in finding anything lurking in your blind spots.</p>
+                <p>Our deals are incredible and they won’t last long, so visit Bev Smith Toyota today at 3350 South US Highway 1, Fort Pierce, FL, to take your test drive. We look forward to putting you in the 2019 Toyota C-HR you’ve always wanted!</p>
+            </div>
+
+        </section>
+        <!-- section -->
+
+        <!-- section -->
+        <section>
+            <div class="column">
+                <h2 class="text-center">Features</h2>
+            </div>
+
+        </section>
+        <!-- section -->
+
+
+        <!-- section -->
+        <section>
+            <div class="column">
+                <h4>DESIGN</h4>
+                <img src="https://www.theautohost.com/_contentPages/vehicleContentPages/toyota/2019/C-HR/images/design.jpg" />
+                <p>Distinctive style meets rebellious spirit in the 2019 Toyota C-HR. Take center stage effortlessly with a precision-cut design that shines from every angle.</p>
+
+            </div>
+
+            <div class="column">
+                <h4>PERFORMANCE
+                </h4>
+                <img src="https://www.theautohost.com/_contentPages/vehicleContentPages/toyota/2019/C-HR/images/performance.jpg" />
+                <p>Powering C-HR is a lively 2.0-liter inline 4-cylinder Dynamic Force Engine with Toyota’s advanced Valvematic technology. Designed for impressive performance and fuel economy, C-HR helps keep the party going with an estimated 27/31 city/highway mpg.19
+
+                </p>
+
+            </div>
+
+        </section>
+        <!-- section -->
+
+
+        <!-- section -->
+        <section class="two">
+            <div class="column">
+                <h4>TECHNOLOGY</h4>
+                <img src="https://www.theautohost.com/_contentPages/vehicleContentPages/toyota/2019/C-HR/images/technology.jpg" />
+                <p>Inside the gauge cluster is Toyota C-HR’s standard 4.2-in. TFT Multi-Information Display, designed to give you all of your go-to information at a glance. See your average mpg, outside temperature, driving modes and trip information.
+
+                </p>
+
+            </div>
+
+            <div class="column">
+                <h4>SAFETY</h4>
+                <img src="https://www.theautohost.com/_contentPages/vehicleContentPages/toyota/2019/C-HR/images/safety.jpg" />
+                <p>Full-Speed Range Dynamic Radar Cruise Control (DRCC)34 takes cruising to the next level. Using forward-sensing radar technology and an intelligent camera, the system is designed to detect the speed and distance of the vehicle traveling directly ahead of you and adjust your speed accordingly, down to a complete stop. It works in light traffic as well, helping you maintain a preset following distance: slowing when the preset distance shortens, then accelerating back to your original cruising speed when the road clears ahead.
+
+                </p>
+
+            </div>
+
+        </section>
+        <!-- section -->
+
+
+
+        <section>
+            <div class="column">
+                <p class="disclaimer">*MSRP Source: Toyota.com</p>
+
+                <p class="disclaimer">1. Estimated mileage. Actual may vary.</p>
+
+                <p class="disclaimer">2. Safety systems do not eliminate the need for safe and attentive driving. See Owner’s Manual for important system details and limitations.</p>
+
+                <p class="disclaimer">19. 2019 EPA-estimated mileage. Actual mileage will vary.</p>
+
+                <p class="disclaimer">34. Full-Speed Range Dynamic Radar Cruise Control is designed to assist the driver and is not a substitute for safe and attentive driving practices. System effectiveness is dependent on road, weather and traffic conditions. See Owner’s Manual for additional limitations and details.</p>
+            </div>
+        </section>
+
+
+
+
+
+
+
+
+    </div>
+
+
+
+
+
+
+
+
+    <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+</body>
+
+</html>
