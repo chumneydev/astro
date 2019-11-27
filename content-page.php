f28fd3a25e50cf35eb6b5b8580e25456a0e3193f
Louis Stephens <lstephens@chumneyads.com>
2019-11-27 17:18:16 -0500

    Mixin Updated
    updating mixins for colors as well as a new cp

---
+++ content-page.php
@@ -0,0 +1,61 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Content Page</title>
+    <link rel="stylesheet" href="bundle/dist/css/cardinal.css">
+    <link rel="stylesheet" href="bundle/dist/css/content-page.css">
+
+
+
+</head>
+
+<body>
+
+    <!-- containter -->
+    <div id="ca-container" class="">
+
+
+
+
+        <section class="header toyota">
+            <div class="column">
+                <h1 class="white text-center">2020 Camry</h1>
+                <p class="white text-center">MSRP $24,295</p>
+                <section>
+                    <div class="column">
+                        <a href="#" class="btn white">View Inventory</a>
+                    </div>
+                    <div class="column">
+                        <a href="#" class="btn white">View Brochure</a>
+                    </div>
+                </section>
+
+
+            </div>
+
+            <div class="column">
+                <img src="bundle/dist/images/car.png" />
+
+            </div>
+
+
+
+        </section>
+
+
+
+
+    </div>
+    <!-- containter -->
+
+    <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+    <script src="./bundle/dist/js/rainbow-custom.min.js"></script>
+    <script src="./bundle/dist/js/cardinal-min.js"></script>
+
+
+
+</body>
+
+</html>
