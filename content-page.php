431a3340f9222efc4b6bff4e618645d5d633df30
Louis Stephens <lstephens@chumneyads.com>
2019-12-03 17:30:34 -0500

    boiling in cp
    cp custom styles added in for all

--- content-page.php
+++ content-page.php
@@ -5,7 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Content Page</title>
     <link rel="stylesheet" href="bundle/dist/css/cardinal.css">
-    <link rel="stylesheet" href="bundle/dist/css/content-page.css">
+    <!--<link rel="stylesheet" href="bundle/dist/css/slick-theme.css">-->
+    <link rel="stylesheet" href="bundle/dist/css/slick.css">
+    <!--<link rel="stylesheet" href="bundle/dist/css/content-page.css">-->
 
 
 
@@ -14,12 +16,12 @@
 <body>
 
     <!-- containter -->
-    <div id="ca-container" class="">
+    <div id="ca-container" class="small">
 
 
 
 
-        <section class="header toyota">
+        <section class="header toyota align-center">
             <div class="column">
                 <h1 class="white text-center">2020 Camry</h1>
                 <p class="white text-center">MSRP $24,295</p>
@@ -36,7 +38,7 @@
             </div>
 
             <div class="column">
-                <img src="bundle/dist/images/car.png" />
+                <img src="bundle/dist/images/car.png" class="ca-image" />
 
             </div>
 
@@ -45,16 +47,169 @@
         </section>
 
 
+        <section>
+            <div class="column">
+                <p>Sed posuere consectetur est at lobortis. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec ullamcorper nulla non metus auctor fringilla. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec ullamcorper nulla non metus auctor fringilla. Sed posuere consectetur est at lobortis.</p>
+
+                <p>Cras mattis consectetur purus sit amet fermentum. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vestibulum id ligula porta felis euismod semper. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</p>
+            </div>
+
+            <div class="column">
+                <!-- slider -->
+                <div class="ca-vehicle-slide toyota">
+
+                    <div>
+                        <div class="vehicle-image">
+                            <h4>L</h4>
+                            <img src="http://www.theautohost.com/_contentPages/vehicleContentPages/toyota/2020/Camry/trims/l.jpg" alt="2020 TOYOTA CAMRY L">
+                        </div>
+                        <div class="vehicle-about">
+                            <p>STARTING MSRP: $24,295<span class="super">1</span></p>
+                            <ul>
+                                <li>Bi-LED combination headlights with auto on/off feature</li>
+                                <li>Audio — includes 7-in. touch-screen with Apple CarPlay<span class="super">&reg;</span><span class="super">52</span> &amp; Android Auto<span class="super">&reg;</span><span class="super">60</span> compatibility</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div class="vehicle-image">
+                            <h4>L</h4>
+                            <img src="http://www.theautohost.com/_contentPages/vehicleContentPages/toyota/2020/Camry/trims/l.jpg" alt="2020 TOYOTA CAMRY L">
+                        </div>
+                        <div class="vehicle-about">
+                            <p>STARTING MSRP: $24,295<span class="super">1</span></p>
+                            <ul>
+                                <li>Bi-LED combination headlights with auto on/off feature</li>
+                                <li>Audio — includes 7-in. touch-screen with Apple CarPlay<span class="super">&reg;</span><span class="super">52</span> &amp; Android Auto<span class="super">&reg;</span><span class="super">60</span> compatibility</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div class="vehicle-image">
+                            <h4>L</h4>
+                            <img src="http://www.theautohost.com/_contentPages/vehicleContentPages/toyota/2020/Camry/trims/l.jpg" alt="2020 TOYOTA CAMRY L">
+                        </div>
+                        <div class="vehicle-about">
+                            <p>STARTING MSRP: $24,295<span class="super">1</span></p>
+                            <ul>
+                                <li>Bi-LED combination headlights with auto on/off feature</li>
+                                <li>Audio — includes 7-in. touch-screen with Apple CarPlay<span class="super">&reg;</span><span class="super">52</span> &amp; Android Auto<span class="super">&reg;</span><span class="super">60</span> compatibility</li>
+                            </ul>
+                        </div>
+                    </div>
+
+
+
+
+                </div>
+                <!-- slider -->
+
+
+
+
+            </div>
+
+
+
+
+        </section>
+
+
+
+        <section>
+            <div class="column">
+                <a href="#" class="btn toyota">Vehicle Specials</a>
+            </div>
+            <div class="column">
+                <a href="#" class="btn toyota">Get Pre-Approved</a>
+            </div>
+            <div class="column">
+                <a href="#" class="btn toyota">Value Your Trade</a>
+            </div>
+            <div class="column">
+                <a href="#" class="btn toyota">Dare to Compare</a>
+            </div>
+
+        </section>
+
+
+        <section class="align-center">
+            <div class="column">
+                <h3 class="ca-title">DESIGN</h3>
+                <h4><b><i>MIDNIGHT BLACK METALLIC ROOF AND ACCENTS</i></b></h4>
+                <p>Take Camry's sporty look to the next level with XSE's available color-contrasting black roof, side mirrors and rear spoiler. The distinctive two-tone Midnight Black Metallic roof 76 highlights the stylish rear roof pillars and adds to the presence of the lower, wider stance.</p>
+            </div>
+
+            <div class="column">
+                <img src="http://www.theautohost.com/_contentPages/vehicleContentPages/toyota/2020/Camry/images/2020-toyota-camry-design.jpg" alt="2020 Toyota Camry Design" class="ca-image" />
+            </div>
+        </section>
+
+
+        <section class="align-center">
+            <div class="column">
+                <img src="http://www.theautohost.com/_contentPages/vehicleContentPages/toyota/2020/Camry/images/2020-toyota-camry-design.jpg" alt="2020 Toyota Camry Design" class="ca-image" />
+            </div>
+            <div class="column">
+                <h3 class="ca-title">DESIGN</h3>
+                <h4><b><i>MIDNIGHT BLACK METALLIC ROOF AND ACCENTS</i></b></h4>
+                <p>Take Camry's sporty look to the next level with XSE's available color-contrasting black roof, side mirrors and rear spoiler. The distinctive two-tone Midnight Black Metallic roof 76 highlights the stylish rear roof pillars and adds to the presence of the lower, wider stance.</p>
+            </div>
+
+        </section>
+
+        <section class="align-center">
+            <div class="column">
+                <h3 class="ca-title">DESIGN</h3>
+                <h4><b><i>MIDNIGHT BLACK METALLIC ROOF AND ACCENTS</i></b></h4>
+                <p>Take Camry's sporty look to the next level with XSE's available color-contrasting black roof, side mirrors and rear spoiler. The distinctive two-tone Midnight Black Metallic roof 76 highlights the stylish rear roof pillars and adds to the presence of the lower, wider stance.</p>
+            </div>
+
+            <div class="column">
+                <img src="http://www.theautohost.com/_contentPages/vehicleContentPages/toyota/2020/Camry/images/2020-toyota-camry-design.jpg" alt="2020 Toyota Camry Design" class="ca-image" />
+            </div>
+        </section>
+
+
+        <section class="align-center">
+            <div class="column">
+                <img src="http://www.theautohost.com/_contentPages/vehicleContentPages/toyota/2020/Camry/images/2020-toyota-camry-design.jpg" alt="2020 Toyota Camry Design" class="ca-image" />
+            </div>
+            <div class="column">
+                <h3 class="ca-title">DESIGN</h3>
+                <h4><b><i>MIDNIGHT BLACK METALLIC ROOF AND ACCENTS</i></b></h4>
+                <p>Take Camry's sporty look to the next level with XSE's available color-contrasting black roof, side mirrors and rear spoiler. The distinctive two-tone Midnight Black Metallic roof 76 highlights the stylish rear roof pillars and adds to the presence of the lower, wider stance.</p>
+            </div>
+
+        </section>
+
+
+
 
 
     </div>
     <!-- containter -->
 
     <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
-    <script src="./bundle/dist/js/rainbow-custom.min.js"></script>
-    <script src="./bundle/dist/js/cardinal-min.js"></script>
+    <script src="bundle/dist/js/rainbow-custom.min.js"></script>
+    <script src="bundle/dist/js/slick.min.js"></script>
+    <script src="bundle/dist/js/cardinal-min.js"></script>
 
-
+    <script type="text/javascript">
+        $(document).ready(function() {
+            // slick carousel
+            $('.ca-vehicle-slide').slick({
+                dots: false,
+                arrows: true,
+                vertical: true,
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                verticalSwiping: true,
+            });
+        });
+    </script>
 
 </body>
 
