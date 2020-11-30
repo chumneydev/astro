615315a1d341f8431407553b3b8607da8f3de4b9
Louis Stephens <lstephens@chumneyads.com>
2020-11-30 12:30:29 -0500

    is-small added to container

---
+++ test.php
@@ -0,0 +1,47 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Cardinal Grid</title>
+    <link rel="stylesheet" href="dist/css/cdl.css">
+    <link rel="stylesheet" href="dist/css/default.css">
+    <link rel="stylesheet" href="dist/css/monokai.css">
+
+
+
+</head>
+
+<body>
+
+    <!-- containter -->
+    <div id="ca-container" class="small">
+
+        <section>
+            <div class="column">
+                <p>Donec id elit non mi porta gravida at eget metus. Nullam quis risus eget urna mollis ornare vel eu leo. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+
+
+
+            </div>
+        </section>
+
+
+
+
+
+
+        <!-- containter -->
+
+        <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+        <script src="dist/js/rainbow-custom.min.js"></script>
+        <script src="dist/js/cardinal-min.js"></script>
+
+
+
+</body>
+<script type="text/javascript">
+
+</script>
+
+</html>
